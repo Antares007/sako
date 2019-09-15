1115bf5a9ef874f019e8ab6108bd34faf7ce6fba
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-09-15 16:48:56 +0400

    fix: lr

--- lr.h
+++ lr.h
@@ -6,39 +6,29 @@
 #include <type_traits>
 #include <variant>
 
-template <typename T> struct NamedArgument {
-  struct argument {
-    template <typename U> T operator=(U &&value) const {
-      return T(std::forward<U>(value));
-    }
-    argument() = default;
-    argument(argument const &) = delete;
-    argument(argument &&) = delete;
-    argument &operator=(argument const &) = delete;
-    argument &operator=(argument &&) = delete;
-  };
-};
-
 namespace lr {
+
 using L = nt::NamedType<std::string, struct LTag>;
-template <typename... Rs> using LR = std::variant<L, Rs...>;
 
-template <typename R> struct leftf {
-  R operator()(L &l) { return R(std::move(l)); }
-};
+template <typename... Rights> using LR = std::variant<L, Rights...>;
 
-template <typename OSet, typename... Rs>
-constexpr decltype(auto) map(OSet &&os, LR<Rs...> &&lr) {
-  using R = decltype(os(std::get<1>(lr)));
-  return std::visit(
-      overloaded{leftf<LR<R>>{}, [&os](auto &&a) { return LR<R>(os(a)); }}, lr);
+template <typename OSet, typename LRType>
+constexpr decltype(auto) map(OSet &&os, LRType &&lr) {
+  using R = decltype(std::declval<OSet>()(std::get<1>(std::declval<LRType>())));
+  return std::visit(overloaded{[](L &&l) { return LR<R>(std::forward<L>(l)); },
+                               [&os](auto &&a) {
+                                 return LR<R>(os(std::forward<decltype(a)>(a)));
+                               }},
+                    std::forward<LRType>(lr));
 }
 
-template <typename OSet, typename... Rs>
-constexpr decltype(auto) flatMap(OSet &&os, LR<Rs...> &&lr) {
-  return std::visit(overloaded{leftf<decltype(os(std::get<1>(lr)))>{},
-                               [&os](auto &&a) { return os(a); }},
-                    lr);
+template <typename OSet, typename LRType>
+constexpr decltype(auto) flatMap(OSet &&os, LRType &&lr) {
+  using R = decltype(std::declval<OSet>()(std::get<1>(std::declval<LRType>())));
+  return std::visit(
+      overloaded{[](L &&l) { return R(std::forward<L>(l)); },
+                 [&os](auto &&a) { return os(std::forward<decltype(a)>(a)); }},
+      std::forward<LRType>(lr));
 }
 
 } // namespace lr
