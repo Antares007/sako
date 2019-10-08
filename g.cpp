b295646e08213c2e92008fe6685405eea90342fd
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-10-08 15:49:55 +0400

    fix

--- g.cpp
+++ g.cpp
@@ -1,16 +1,24 @@
 #include <git2.h>
 #include <memory>
 
+namespace abo {
+template <class... O> struct o : O... { using O::operator()...; };
+template <class... O> o(O...)->o<O...>;
+template <typename... Ts> struct print;
+} // namespace abo
+
 namespace abo::git {
 
-template <typename T, typename... Args> struct lift;
-template <typename T, typename... Args> struct lift<T **, Args...> {
+template <typename... Args> struct lift;
+template <typename T, typename... Args> struct lift<T *, Args...> {
   int (*ctor)(T **, Args...);
   void (*dtor)(T *);
-  template <typename O>
-  constexpr void operator()(Args... args, const O &o) const {
+  template <typename O, typename = std::enable_if_t<std::conjunction_v<
+                            std::is_invocable_r<void, O, int>,
+                            std::is_invocable_r<void, O, T *>>>>
+  constexpr void operator()(const O &o, Args &&... args) const {
     T *ptr = nullptr;
-    if (int error = ctor(&ptr, args...))
+    if (int error = ctor(&ptr, std::move(args)...))
       o(error);
     else {
       o(ptr);
@@ -18,12 +26,15 @@ template <typename T, typename... Args> struct lift<T **, Args...> {
     }
   }
 };
-template <typename T, typename... Args> struct lift<T *, Args...> {
-  int (*ctor)(T *, Args...);
-  template <typename O>
-  constexpr void operator()(Args... args, const O &o) const {
-    T t = nullptr;
-    if (int error = ctor(&t, args...))
+template <typename T, typename... Args> struct lift<T, Args...> {
+  int (*f)(T *, Args...);
+  template <
+      typename O,
+      typename = std::enable_if_t<std::conjunction_v<
+          std::is_invocable_r<void, O, int>, std::is_invocable_r<void, O, T>>>>
+  constexpr void operator()(const O &o, Args... args) const {
+    T t;
+    if (int error = f(&t, std::move(args)...))
       o(error);
     else {
       o(std::move(t));
@@ -31,9 +42,9 @@ template <typename T, typename... Args> struct lift<T *, Args...> {
   }
 };
 template <typename T, typename... Args>
-lift(int (*)(T **, Args...), void (*)(T *))->lift<T **, Args...>;
+lift(int (*)(T **, Args...), void (*)(T *))->lift<T *, Args...>;
 template <typename T, typename... Args>
-lift(int (*)(T *, Args...))->lift<T *, Args...>;
+lift(int (*)(T *, Args...))->lift<T, Args...>;
 
 template <typename Pith> struct bark {
   Pith pith;
@@ -43,14 +54,23 @@ template <typename Pith> struct bark {
 
   template <typename O,
             typename = std::enable_if_t<std::is_invocable_r_v<void, O, int>>>
-  constexpr void operator()(git_repository *repo, O &&) const {}
+  constexpr void operator()(git_repository *, O &&) const {}
 };
 template <typename Pith> bark(Pith)->bark<Pith>;
 } // namespace abo::git
 
 int main() {
-  //
+  git_libgit2_init();
+
   auto a = abo::git::lift{git_repository_open, git_repository_free};
-  auto b = abo::git::bark{[](int) {}};
+  a(abo::o{[](int) {},
+           [](git_repository *) {
+             //
+           }},
+    "");
+
+  auto c = abo::git::lift{git_oid_fromstr};
+  c(abo::o{[](int) {}, [](git_oid) {}}, "");
+
   return 3;
 }
