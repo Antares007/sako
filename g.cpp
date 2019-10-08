9709903597f83e4b43fb8c2727d56e163c3ff1e8
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-10-08 14:05:14 +0400

    lift

--- g.cpp
+++ g.cpp
@@ -1,49 +1,56 @@
 #include <git2.h>
 #include <memory>
 
-namespace abo {
+namespace abo::git {
 
-namespace git {
-template <typename T> using UPtr = std::unique_ptr<T, void (*)(T *)>;
-
-namespace detail {
-template <typename T> constexpr const char *p(T &&) {
-  return __PRETTY_FUNCTION__;
-}
-
-template <typename T, typename... Args>
-constexpr auto m1(int (*c)(T **, Args...), void (*d)(T *)) {
-  return [c, d](Args... args, auto o) {
+template <typename T, typename... Args> struct lift;
+template <typename T, typename... Args> struct lift<T **, Args...> {
+  int (*ctor)(T **, Args...);
+  void (*dtor)(T *);
+  template <typename O>
+  constexpr void operator()(Args... args, const O &o) const {
     T *ptr = nullptr;
-    if (int error = c(&ptr, args...))
-      o(p(c));
-    else
-      o(UPtr<T>(ptr, d));
-  };
-}
-
+    if (int error = ctor(&ptr, args...))
+      o(error);
+    else {
+      o(ptr);
+      dtor(ptr);
+    }
+  }
+};
+template <typename T, typename... Args> struct lift<T *, Args...> {
+  int (*ctor)(T *, Args...);
+  template <typename O>
+  constexpr void operator()(Args... args, const O &o) const {
+    T t = nullptr;
+    if (int error = ctor(&t, args...))
+      o(error);
+    else {
+      o(std::move(t));
+    }
+  }
+};
 template <typename T, typename... Args>
-constexpr auto m2(int (*c)(T *, Args...)) {
-  return [c](Args... args, auto o) {
-    T v = {};
-    if (int error = c(&v, args...))
-      o(p(c));
-    else
-      o(std::move(v));
-  };
-}
-} // namespace detail
+lift(int (*)(T **, Args...), void (*)(T *))->lift<T **, Args...>;
+template <typename T, typename... Args>
+lift(int (*)(T *, Args...))->lift<T *, Args...>;
 
-constexpr inline auto repository_open =
-    detail::m1(git_repository_open, git_repository_free);
-constexpr inline auto tree_lookup = detail::m1(git_tree_lookup, git_tree_free);
-constexpr inline auto oid_fromstr = detail::m2(git_oid_fromstr);
-} // namespace git
+template <typename Pith> struct bark {
+  Pith pith;
+  template <typename U,
+            typename = std::enable_if_t<std::is_invocable_r_v<void, U, int>>>
+  bark(U &&) {}
 
-} // namespace abo
+  template <typename O,
+            typename = std::enable_if_t<std::is_invocable_r_v<void, O, int>>>
+  constexpr void operator()(git_repository *repo, O &&) const {}
+};
+template <typename Pith> bark(Pith)->bark<Pith>;
+} // namespace abo::git
 
 int main() {
   //
-  int iaa = 1;
+  auto a = abo::git::lift{git_repository_open, git_repository_free};
+  auto b = abo::git::bark{[](int) {}};
   return 3;
 }
