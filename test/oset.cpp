93f215f6af29db62abae7b1932141711038a3fa9
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-10-11 12:55:25 +0400

    sync

--- test/oset.cpp
+++ test/oset.cpp
@@ -2,64 +2,6 @@
 
 using namespace abo;
 
-template <typename T> using UPtr = std::unique_ptr<T, void (*)(T *)>;
-struct L {
-  L() = delete;
-  L(const L &) = delete;
-  L(L &&) = default;
-  template <typename T,
-            typename = std::enable_if_t<std::is_convertible_v<T, std::string>>>
-  L(T &&a) : message(std::forward<T>(a)) {}
-  L operator+(const L &other) { return L(this->message + other.message); }
-  std::string message;
-};
-
-template <typename T, typename... Args>
-constexpr auto m1(int (*c)(T **, Args...), void (*d)(T *)) {
-  return [c, d](Args... args, auto o) {
-    T *ptr = nullptr;
-    if (int error = c(&ptr, args...))
-      o(L{""});
-    else
-      o(UPtr<T>(ptr, d));
-  };
-}
-
-template <typename T, typename... Args>
-constexpr auto m2(int (*c)(T *, Args...)) {
-  return [c](Args... args, auto o) {
-    T v = {};
-    if (int error = c(&v, args...))
-      o(L{""});
-    else
-      o(std::move(v));
-  };
-}
-
-constexpr decltype(auto) open = m1(git_repository_open, git_repository_free);
-// constexpr decltype(auto) oid_fromstr = m2(git_oid_fromstr);
-
-template <typename A, typename F>
-constexpr auto operator|(A &&a, F &&f) -> decltype(f(std::forward<A>(a))) {
-  return f(std::forward<A>(a));
-}
-
-#define G(T, O) m1(T##_##O, T##_free, G_NEXT_
-#define G_NEXT_(...) __VA_ARGS__)
-
-template <typename... Fns> constexpr decltype(auto) o(Fns &&... fns) {
-  return overloaded{[](int err) { std::cout << "Error" << err << '\n'; },
-                    std::forward<Fns>(fns)...};
-};
-
-NewType(A, int);
-NewType(B, int);
-NewType(O, int);
-
-template <typename Pith, typename... Rays>
-constexpr inline auto is_pith_v =
-    std::is_invocable_r_v<void, Pith, abo::o<Rays...>>;
-
 template <typename F> struct proxy_ray {
   F f;
   template <typename U>
@@ -74,7 +16,7 @@ struct any_ray {
   template <typename U> constexpr void operator()(U &&) const {};
 };
 
-template <typename F> struct fmap {
+template <typename L, typename F> struct fmap {
   F f;
   template <typename Pith, typename = std::enable_if_t<std::is_invocable_r_v<
                                void, Pith, abo::o<abo::ray<L>, proxy_ray<F>>>>>
@@ -95,9 +37,9 @@ template <typename F> struct fmap {
 };
 template <typename F> fmap(F)->fmap<F>;
 
-template <typename Pith, typename... T> struct variant_b {
-  variant_b(Pith) {}
-};
+NewType(A, int);
+NewType(B, int);
+NewType(O, int);
 
 int main() {
   auto p =
