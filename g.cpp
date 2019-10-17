1b2e71316d065cfacd3556f040dbca79b185ebaa
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-10-17 11:04:13 +0400

    sym

--- g.cpp
+++ g.cpp
@@ -22,28 +22,26 @@ template <typename... Args> struct lift;
 template <typename T, typename... Args> struct lift<T *, Args...> {
   int (*ctor)(T **, Args...);
   void (*dtor)(T *);
-  template <typename O> auto operator()(Args... args, O &&o) const -> int {
+  template <typename O> void operator()(Args... args, O &&o) const {
     T *pv = nullptr;
-    int e = ctor(&pv, args...);
-    if (e)
-      return e;
-    o(pv);
-    dtor(pv);
-    return e;
+    if (int e = ctor(&pv, args...))
+      o(e);
+    else {
+      o(pv);
+      dtor(pv);
+    }
   }
 };
 
 template <typename T, typename... Args> struct lift<T, Args...> {
   int (*ctor)(T *, Args...);
-  template <typename O> auto operator()(Args... args, O &&o) const -> int {
-    T v;
-    int e = ctor(&v, args...);
-    if (e)
-      return e;
-    o(v);
-    return e;
+  template <typename O> void operator()(Args... args, O &&o) const {
+    T v{};
+    if (int e = ctor(&v, args...))
+      o(e);
+    else
+      o(v);
   }
-
 };
 
 template <typename T, typename... Args>
@@ -51,19 +49,14 @@ lift(int (*)(T **, Args...), void (*)(T *))->lift<T *, Args...>;
 template <typename T, typename... Args>
 lift(int (*)(T *, Args...))->lift<T, Args...>;
 
-#define LFTP(T, O)                                                             \
-  constexpr inline auto T##_##O = abo::git::lift {                             \
-    git_##T##_##O, git_##T##_free                                              \
-  }
-#define LFT(T, O)                                                              \
-  constexpr inline auto T##_##O = abo::git::lift { git_##T##_##O }
+inline auto repository_open =
+    abo::git::lift{git_repository_open, git_repository_free};
+inline auto tree_lookup = abo::git::lift{git_tree_lookup, git_tree_free};
+inline auto treebuilder_new =
+    abo::git::lift{git_treebuilder_new, git_treebuilder_free};
 
-LFTP(repository, open);
-LFTP(tree, lookup);
-LFTP(treebuilder, new);
-
-LFT(treebuilder, write);
-LFT(oid, fromstr);
+inline auto treebuilder_write = abo::git::lift{git_treebuilder_write};
+inline auto oid_fromstr = abo::git::lift{git_oid_fromstr};
 
 template <typename... T> struct ray;
 template <> struct ray<> {
@@ -75,18 +68,12 @@ struct ray<T, Rest...> : ray<T>, ray<Rest...> {
   using ray<T>::operator();
   using ray<Rest...>::operator();
 };
+
 template <typename Pith> struct bark {
   Pith pith;
 
-  template <typename U> explicit bark(U &&u) : pith(std::forward<U>(u)) {}
-
   auto operator()(int i) const { return i; }
-  auto operator()(git_repository *repo) const {
-    return pith(abo::o{[](const char *name, git_filemode_t mode, git_oid id) {
-                  //
-                }},
-                repo);
-  }
+  auto operator()(git_repository *) const {}
 };
 template <typename Pith> bark(Pith)->bark<Pith>;
 } // namespace abo::git
@@ -94,37 +81,28 @@ template <typename Pith> bark(Pith)->bark<Pith>;
 #include <iostream>
 using namespace abo;
 
-int main() {
+
+auto main() -> int {
   git_libgit2_init();
 
-  auto see =
-      git::repository_open(".", git::bark([](auto o, git_repository *) { //
-                             git_oid id;
-                             if (int err = git_oid_fromstr(&id, ""))
-                               return err;
-                           }));
+  auto repoPith = std::bind_front(git::repository_open, ".");
+  repoPith(abo::o{[](int) {}, [](git_repository *) {}});
 
   (abo::o{[](int) {},
           [](auto id) { std::cout << git_oid_tostr_s(&id) << '\n'; }}) |
       [](auto o) {
         auto l = [&](int i) { o(i); };
-        git::repository_open(
-            ".",
-            abo::o{
-                l, [&](auto repo) {
-                  git::oid_fromstr(
-                      "f5880cf63a4372dcafb791620731637b4130d9df",
-                      abo::o{
-                          l, [&](auto oid) {
-                            std::cout << "abo\n";
-                            git::tree_lookup(
-                                repo, &oid,
-                                abo::o{
-                                    l, [&](auto tree) {
-                                      git::treebuilder_new(
-                                          repo, nullptr,
-                                          abo::o{
-                                              l, [&](auto bld) {
+        std::bind_front(git::repository_open, ".")(abo::o{
+            l, [&](auto repo) {
+              std::bind_front(
+                  git::oid_fromstr,
+                  "f5880cf63a4372dcafb791620731637b4130d9df")(abo::o{
+                  l, [&](auto oid) {
+                    std::bind_front(git::tree_lookup, repo, &oid)(abo::o{
+                        l, [&](auto tree) {
+                          std::bind_front(
+                              git::treebuilder_new, repo,
+                              nullptr)(abo::o{l, [&](auto bld) {
                                                 auto count =
                                                     git_tree_entrycount(tree);
                                                 for (size_t i = 1; i < count;
@@ -141,9 +119,9 @@ int main() {
                                                 }
                                                 git::treebuilder_write(bld, o);
                                               }});
-                                    }});
-                          }});
-                }});
+                        }});
+                  }});
+            }});
       };
 
   return 3;
