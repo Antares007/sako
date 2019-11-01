86a534b923531b8d807721cc95f217ca761b5fdd
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-11-01 12:46:38 +0400

    fix

--- parsec.hpp
+++ parsec.hpp
@@ -4,44 +4,76 @@
 #include <functional>
 namespace next {
 
+/// 0xxxxxxx
+/// 110xxxxx	10xxxxxx
+/// 1110xxxx	10xxxxxx	10xxxxxx
+/// 11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
 struct u8cp {
-  const char *s;
-  size_t avail;
   MO()(const char *s, size_t avail) {
-    if (avail == 0)
-      return o(-1);
-    if (0x00 == (0x80 & s[0]))
-      o(1, s[0]);
-    else if (0xc0 == (0xe0 & s[0]))
-      if (avail < 2)
-        o(-1);
-      else
-        o(2, ((0x1f & s[0]) << 6) | (0x3f & s[1]));
-    else if (0xe0 == (0xf0 & s[0]))
-      if (avail < 3)
-        o(-1);
-      else
-        o(3, ((0x0f & s[0]) << 12) | ((0x3f & s[1]) << 6) | (0x3f & s[2]));
-    else if (0xf0 == (0xf8 & s[0]))
-      if (avail < 4)
-        o(-1);
-      else
-        o(4, ((0x07 & s[0]) << 18) | ((0x3f & s[1]) << 12) |
-                 ((0x3f & s[2]) << 6) | (0x3f & s[3]));
-    else
+    if (avail < 1)
       o(-1);
+    else if (((uint8_t)s[0]) >> 7 == 0)
+      o(1);
+    else if (avail < 2 || ((uint8_t)s[1]) >> 6 != 0b10)
+      o(-2);
+    else if (((uint8_t)s[0]) >> 5 == 0b110)
+      o(2);
+    else if (avail < 3 || ((uint8_t)s[2]) >> 6 != 0b10)
+      o(-3);
+    else if (((uint8_t)s[0]) >> 4 == 0b1110)
+      o(3);
+    else if (avail < 4 || ((uint8_t)s[3]) >> 6 != 0b10)
+      o(-4);
+    else if (((uint8_t)s[0]) >> 3 == 0b11110)
+      o(4);
+    else
+      o(-5);
   }
 };
+
+namespace detail {
+inline uint32_t cp(const char *in, int x) {
+  return x == 1 ? in[0]
+                : x == 2 ? ((0x1f & in[0]) << 6) | (0x3f & in[1])
+                         : x == 3 ? ((0x0f & in[0]) << 12) |
+                                        ((0x3f & in[1]) << 6) | (0x3f & in[2])
+                                  : ((0x07 & in[0]) << 18) |
+                                        ((0x3f & in[1]) << 12) |
+                                        ((0x3f & in[2]) << 6) | (0x3f & in[3]);
+}
+} // namespace detail
 
 template <uint32_t C, uint32_t... Cs> struct chr {
   static constexpr bool hasU8cp = ((C > 127) || ... || (Cs > 127));
   MO()(const char *in, const size_t size) {
     if constexpr (hasU8cp)
-      u8cp{in, size}(_o_{o, [&](int s, uint32_t cp) {
-                           o(((C == cp) || ... || (Cs == cp)) ? s : -1);
-                         }});
+      u8cp{}(
+          [&](int x) {
+            if (x < 0)
+              o(x);
+            else {
+              const uint32_t cp = detail::cp(in, x);
+              o(((C == cp) || ... || (Cs == cp)) ? x : -1);
+            }
+          },
+          in, size);
     else
       o(size > 0 && ((C == in[0]) || ... || (Cs == in[0])) ? 1 : -1);
+  }
+};
+
+template <uint32_t C, uint32_t... Cs> struct nchr {
+  MO()(const char *in, const size_t size) { //
+    u8cp{}(
+        [&](int x) {
+          if (x < 0)
+            o(x);
+          else {
+            const uint32_t cp = detail::cp(in, x);
+            o(((C != cp) && ... && (Cs != cp)) ? x : -1);
+          }
+        },
+        in, size);
   }
 };
 
@@ -49,8 +81,16 @@ template <uint32_t...> struct rng;
 template <uint32_t A, uint32_t B> struct rng<A, B> {
   MO()(const char *in, const size_t size) {
     if constexpr (A > 127 || B > 127)
-      u8cp{in, size}(
-          _o_{o, [&](int s, uint32_t cp) { o(A <= cp && cp <= B ? s : -1); }});
+      u8cp{}(
+          [&](int x) {
+            if (x < 0)
+              o(x);
+            else {
+              const uint32_t cp = detail::cp(in, x);
+              o(A <= cp && cp <= B ? x : -1);
+            }
+          },
+          in, size);
     else
       o(size > 0 && A <= in[0] && in[0] <= B ? 1 : -1);
   }
@@ -67,6 +107,7 @@ template <uint32_t A, uint32_t B, uint32_t... Rest> struct rng<A, B, Rest...> {
         in, size);
   }
 };
+
 template <typename A, typename B> struct or_ {
   A a;
   B b;
@@ -81,6 +122,7 @@ template <typename A, typename B> struct or_ {
         in, avail);
   };
 };
+
 template <typename A, typename B> struct and_ {
   A a;
   B b;
@@ -95,6 +137,7 @@ template <typename A, typename B> struct and_ {
         in, avail);
   };
 };
+
 template <typename A> struct many {
   A a;
   MO()(const char *in, size_t avail, size_t acc = 0) {
