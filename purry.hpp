ef427461c180789f82562194a751ba4d36bde7f6
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-11-16 20:06:26 +0400

    use concepts

--- purry.hpp
+++ purry.hpp
@@ -6,7 +6,7 @@ template <typename Pith> purry(Pith)->purry<Pith>;
 template <typename Pith> struct purry {
   Pith pith;
   template <typename O> auto operator()(O &&o) const {
-    if constexpr (std::is_invocable_r_v<void, Pith, O>)
+    if constexpr (!!invocable<Pith, O>)
       pith(static_cast<O &&>(o));
     else
       return purry<pin<Pith, O>>{pin<Pith, O>{pith, static_cast<O &&>(o)}};
@@ -15,23 +15,21 @@ template <typename Pith> struct purry {
 template <typename Pith, typename A> struct purry<pin<Pith, A>> {
   pin<Pith, A> pith;
   template <typename O> auto operator()(O &&o) const {
-    if constexpr (std::is_invocable_r_v<void, Pith, O, A>) {
+    if constexpr (!!invocable<Pith, O, A>)
       pith(static_cast<O &&>(o));
-    } else {
+    else
       return purry<pin<pin<Pith, A>, O>>{
           pin<pin<Pith, A>, O>{pith, static_cast<O &&>(o)}};
-    }
   }
 };
 template <typename Pith, typename A, typename B>
 struct purry<pin<pin<Pith, A>, B>> {
   pin<pin<Pith, A>, B> pith;
   template <typename O> auto operator()(O &&o) const {
-    if constexpr (std::is_invocable_r_v<void, Pith, O, A, B>)
+    if constexpr (!!invocable<Pith, O, A, B>)
       pith(static_cast<O &&>(o));
-    else {
+    else
       return purry<pin<pin<pin<Pith, A>, B>, O>>{
           pin<pin<pin<Pith, A>, B>, O>{pith, static_cast<O &&>(o)}};
-    }
   }
 };
