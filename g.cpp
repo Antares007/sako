60fb885fc6bd9c4672ee5860103bfa2c53c3a12f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-11-23 15:42:53 +0400

    extract

--- g.cpp
+++ g.cpp
@@ -21,7 +21,19 @@ int main() {
     o("ABO");
     o([&](auto o) { o(fib(8)); });
 
-    auto step1 = git::tree_bark{[](auto self, auto o, auto r, auto treeid) {
+    auto xlsx2tree = git::tree_bark{[](auto, auto o, auto r, auto blob) {
+      o(purry{unzip, git_blob_rawcontent(blob), git_blob_rawsize(blob)} |
+        [&](auto o, auto n, auto b, auto s) {
+          o(git::blob_create_frombuffer ^ r ^ b ^ s | [&](auto o, git_oid *id) {
+            auto name = std::string(n);
+            for (size_t i = 0; i < name.size(); i++)
+              if (name[i] == '/')
+                name[i] = '_';
+            o(name.c_str(), id, git::BLOB);
+          });
+        });
+    }};
+    auto step1 = git::tree_bark{[&](auto self, auto o, auto r, auto treeid) {
       o(git::ls ^ (git::tree_lookup ^ r ^ treeid) | [&](auto o, auto name,
                                                         auto oid, auto mode) {
         if (mode == git::TREE)
@@ -31,21 +43,7 @@ int main() {
               o(name, oid, git::TREE);
           });
         else if (mode == git::BLOB && std::string_view(name).ends_with(".xlsx"))
-          o(purry{git::tree_bark{[](auto, auto o, auto r, auto blob) {
-                    o(purry{unzip, git_blob_rawcontent(blob),
-                            git_blob_rawsize(blob)} |
-                      [&](auto o, auto n, auto b, auto s) {
-                        o(git::blob_create_frombuffer ^ r ^ b ^ s |
-                          [&](auto o, git_oid *id) {
-                            auto name = std::string(n);
-                            for (size_t i = 0; i < name.size(); i++)
-                              if (name[i] == '/')
-                                name[i] = '_';
-                            o(name.c_str(), id, git::BLOB);
-                          });
-                      });
-                  }},
-                  r, git::blob_lookup ^ r ^ oid} |
+          o(purry{xlsx2tree, r, git::blob_lookup ^ r ^ oid} |
             [&](auto o, auto oid) { o(name, oid, git::TREE); });
       });
     }};
