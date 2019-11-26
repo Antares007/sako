5f4e8c93ab1ffc481fa2e17c33ae3f59a9d81fe4
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-11-26 08:43:11 +0400

    show

--- show.cpp
+++ show.cpp
@@ -1,19 +1,35 @@
 #include "purry.hpp"
-struct L;
 #include <iostream>
-
-constexpr inline auto out = _o_{[](int err) { std::cerr << err << '\n'; },
-                                [](auto cstr) { std::cout << cstr << '\n'; }};
 
 struct A {};
 struct B {};
 struct C {};
+struct D {};
 
 int main() {
+  auto compose = [](auto f, auto g) {
+    return purry{[=](auto o, auto a) { o(g ^ (f ^ a)); }};
+  };
+
   auto f = purry{[](auto o, A) { o(B{}); }};
   auto g = purry{[](auto o, B) { o(C{}); }};
+  auto h = purry{[](auto o, C) { o(D{}); }};
 
-  auto gof = g ^ (f ^ A{});
+  auto gof_1 = compose(compose(f, g), h);
+  auto gof_2 = compose(f, compose(g, h));
 
-  gof([](auto) {});
+  (gof_1 ^ A{})(_o_{[](int) {}, [](D) {}});
+  (gof_2 ^ A{})(_o_{[](int) {}, [](D) {}});
+
+  auto compose_ = [](auto f, auto g) {
+    return [=](auto a) { return g(f(a)); };
+  };
+  auto f_ = [](A) { return B{}; };
+  auto g_ = [](B) { return C{}; };
+  auto h_ = [](C) { return D{}; };
+
+  auto gofoh_ = compose_(compose_(f_, g_), h_);
+  auto r_ = gofoh_(A{});
+
+  static_assert(std::is_same_v<decltype(r_), D>);
 }
