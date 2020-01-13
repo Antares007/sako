107236deec60a3a46e1ec77ceb23379c517d80be
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-01-13 18:34:00 +0400

    parse prolog

--- p.cpp
+++ p.cpp
@@ -7,16 +7,16 @@ using namespace parsec::xml;
 
 int main() {
   //
-  auto a = R"xml(<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+  const char *xmltext =
+      R"xml(<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="145" uniqueCount="67"><si><t>Azkot Hotel</t></si><si><t>Assam hotel</t></si><si><t>Astoria Tbilisi</t></si><si><t>Vardzia Resort</t></si><si><t>Silk road Hotel</t></si><si><t>laertoni</t></si><si><t>raodenoba</t></si><si><t>Mr Paul Croft</t></si><si><t>mail</t></si><si><t>paulcroft@madasafish.com</t></si><si><t>agent</t></si><si><t>tbilisi</t></si><si><t>bako</t></si><si><t>sheki</t></si><si><t>vardzia</t></si><si><t>erevan</t></si><si><t>Azerbaijan, Georgia and Armenia 12 days tour</t></si><si><t>Start Date</t></si><si><t>End Date</t></si><si><t>Total Travelers</t></si><si><t>Quantity</t></si><si><t>bbb</t></si><si><t>dddd</t></si><si><t>Date</t></si><si><t>Location</t></si><si><t>axalcixe</t></si><si><t>erevani</t></si><si><t>departure</t></si><si><t>Hotels</t></si><si><t>In</t></si><si><t>Out</t></si><si><t>Twin</t></si><si><t>Double</t></si><si><t>Single</t></si><si><t>Dextra</t></si><si><t>aaaa</t></si><si><t>Baku</t></si><si><t>Sheki</t></si><si><t>Tbilisi</t></si><si><t>Vardzia</t></si><si><t>Yerevan</t></si><si><t>Name</t></si><si><t>sheki driver and guide</t></si><si><t>vardzia driver and guide</t></si><si><t>Contact</t></si><si><t>Country</t></si><si><t>Azerbaijan</t></si><si><t>Georgia</t></si><si><t>Armenia</t></si><si><t>Confirmation mail</t></si><si><t>DateStr</t></si><si><t>Day</t></si><si><t>Triple</t></si><si><t>Driver/Name/Contact</t></si><si><t>Extra</t></si><si><t>Dates &amp; Time</t></si><si><t>Flight #</t></si><si><t>From</t></si><si><t>To</t></si><si><t>Driver</t></si><si><t>Hotel</t></si><si><t>Gude</t></si><si><t>Restaurants</t></si><si><t>Entrance Fee</t></si><si><t>N</t></si><si><t>ExHotels</t></si><si><t>ExTransfer</t></si></sst>)xml";
 
-  auto text = "<!--  -->";
-
-  (Comment, text)(::rays{[](error_ray *, int err) { //
-                           std::cout << "error: " << err << '\n';
-                         },
-                         [](int len) { //
-                           std::cout << "len: " << len << '\n';
-                         }});
+  (document, xmltext)(::rays{
+      [](error_ray *, int err) { std::cout << "error: " << err << '\n'; },
+      [&](int len) {
+        std::cout << "len: " << len << '\n'
+                  << xmltext[len] << '\n'
+                  << xmltext + len;
+      }});
   return 9;
 }
--- xml.hpp
+++ xml.hpp
@@ -23,7 +23,7 @@ C NameStartChar =
              (0xFDF0 <= cp && cp <= 0xFFFD) || (0x10000 <= cp && cp <= 0xEFFFF);
     }};
 
-// NameChar	    ::= NameStartChar | "-" | "." | [0-9] | #xB7 |
+// NameChar	 ::= NameStartChar | "-" | "." | [0-9] | #xB7 |
 // [#x0300-#x036F] | [#x203F-#x2040]
 C NameChar =
     NameStartChar |
@@ -33,24 +33,49 @@ C NameChar =
              (0x203F <= cp && cp <= 0x2040);
     }};
 
-// Name	        ::= NameStartChar (NameChar)*
+// Name	 ::= NameStartChar (NameChar)*
 C Name = NameStartChar & many{0, NameChar};
 
-// Comment      ::= '<!--' ((Char - '-') | ('-' (Char - '-')))* '-->'
+// S ::= (#x20 | #x9 | #xD | #xA)+
+C S = many{1, chr{[](char c) {
+             return c == 0x20 || c == 0x09 || c == 0x0D || c == 0x0A;
+           }}};
+
+// Comment ::= '<!--' ((Char - '-') | ('-' (Char - '-')))* '-->'
 C Comment =
     str{"<!--"} & (many{0, noneOf{"-"} | str{"-"} & noneOf{"-"}}) & str{"-->"};
 
-// content      ::= CharData? ((element | Reference | CDSect | PI | Comment)
+// content ::= CharData? ((element | Reference | CDSect | PI | Comment)
 // CharData?)*
 
-// AttValue     ::= '"' ([^<&"] | Reference)* '"' |  "'" ([^<&'] |
+// AttValue ::= '"' ([^<&"] | Reference)* '"' | "'" ([^<&'] |
 // Reference)* "'"
 
-// Attribute    ::= Name Eq AttValue
-// ETag         ::= '</' Name S? '>'
-// STag         ::= '<' Name (S Attribute)* S? '>'
+// Attribute ::= Name Eq AttValue
+// ETag ::= '</' Name S? '>'
+// STag ::= '<' Name (S Attribute)* S? '>'
 // EmptyElemTag ::= '<' Name (S Attribute)* S? '/>'
-// element      ::= EmptyElemTag | STag content ETag
-// document     ::= prolog element Misc*
+
+// element ::= EmptyElemTag | STag content ETag
+C element = [](auto o, const char *) { o(0); };
+
+// PI ::= '<?' PITarget (S (Char* - (Char* '?>' Char*)))? '?>'
+C PI = str{"<?"} & many{0, noneOf{"?"}} & str{"?>"};
+
+// Misc ::= Comment | PI | S
+C Misc = Comment | PI | S;
+
+// XMLDecl ::= '<?xml' VersionInfo EncodingDecl? SDDecl? S? '?>'
+C XMLDecl = str{"<?xml"} & many{0, noneOf{"?"}} & str{"?>"};
+
+// doctypedecl ::='<!DOCTYPE' S Name (S ExternalID)? S? ('[' intSubset ']' S?)?
+// '>'
+C doctypedecl = str{"<!DOCTYPE"} & many{0, noneOf{">"}} & str{">"};
+
+// prolog ::= XMLDecl? Misc* (doctypedecl Misc*)?
+C prolog = opt{XMLDecl} & many{0, Misc} & opt{doctypedecl & many{0, Misc}};
+
+// document ::= prolog element Misc*
+C document = prolog & element & many{0, Misc};
 
 } // namespace parsec::xml
