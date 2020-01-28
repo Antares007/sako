f49a2f9b52e72c9839b4793c0080c0ccc7b85b87
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-01-28 10:24:52 +0400

    sync

--- a.cpp
+++ a.cpp
@@ -11,6 +11,7 @@ struct list_pith {
     tail(*this);
   }
 };
+constexpr inline auto nil = [](const auto &) {};
 
 constexpr inline auto sample_list = [](auto o) {
   o(head_ray_v, 3);
@@ -21,22 +22,36 @@ constexpr inline auto sample_list = [](auto o) {
 };
 template <typename...> struct list;
 template <typename... Args> list(Args...) -> list<Args...>;
-
 template <typename H> struct list<H> {
   H h;
+  template <typename U> list(U &&u) : h(static_cast<U &&>(u)) {}
   template <typename O> void operator()(const O &o) const { o(head_ray_v, h); }
 };
-template <typename H, typename T> struct list<H, list<T>> {
+template <typename H, typename... T> struct list<H, T...> : list<T...> {
   H h;
-  list<T> t;
+  template <typename U, typename... V>
+  list(U &&u, V &&... v)
+      : list<T...>(static_cast<V &&>(v)...), h(static_cast<U &&>(u)) {}
   template <typename O> void operator()(const O &o) const {
     o(head_ray_v, h);
-    o(tail_ray_v, t);
+    o(tail_ray_v, [&](const auto &o) { list<T...>::operator()(o); });
   }
 };
-template <typename H, typename T, typename... Rest>
-void test(H h, T t, Rest... rest) {}
+
+#include <iostream>
+
+struct log_list_pith {
+  template <typename H> void operator()(head_ray *, H &&h) const {
+    std::cout << h << '\n';
+  }
+  template <typename T> void operator()(tail_ray *, T &&tail) const {
+    std::cout << "next" << '\n';
+    tail(*this);
+  }
+};
 int main() {
-  list{1, list{1}}(list_pith{});
+  sample_list(list_pith{});
+  auto l = list{"a", 2, 3.3f, 'o'};
+  l(log_list_pith{});
   return 9;
 }
--- lalr/g41.hpp
+++ lalr/g41.hpp
@@ -1,17 +1,17 @@
 #pragma once
 #include "terminals.hpp"
 #define Derives template <typename O> void operator()(const O &o) const
-
+constexpr inline auto nil = [](const auto &) {};
+constexpr inline auto cons = [](auto...) { return 1; };
+struct head;
+struct tail;
+#define P(...)
 struct expr {
   Derives {
-    o([](auto o) {
-      o(expr{});
-      o([](auto o) { //
-        o(PLUS{});
-      });
-    });
-    o(expr{} >>= PLUS{} >>= term{});
-    o(term{});
+    cons(                                              //
+        cons(expr{}, cons(PLUS{}, cons(term{}, nil))), //
+        cons(cons(term{}, nil), nil)                   //
+        )(o);
   }
   struct term {
     Derives {
