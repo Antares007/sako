30dddf3c9dfde5a5f946c525f45f4df376da8d91
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-02-06 10:58:04 +0400

    sync regular grammar

--- b.cpp
+++ b.cpp
@@ -2,29 +2,71 @@
 #include "purry.hpp"
 #include <iostream>
 
-constexpr inline auto l = [](const auto &o) {
-  o(head_ray_v, 1, [&](const auto &o) {
-    o(head_ray_v, 2, [&](const auto &o) { o(head_ray_v, 3, nullptr); });
-  });
+template <typename... Ts> struct print;
+
+template <typename L, typename R> struct or_ {
+  L l;
+  R r;
+  void operator()(Car o, const char *in) const {
+    l(
+        [&](int len) {
+          if (len < 0)
+            r(o, in);
+          else
+            o(len);
+        },
+        in);
+  }
+};
+template <typename L, typename R> or_(L, R) -> or_<L, R>;
+
+template <typename L, typename R> struct and_ {
+  L l;
+  R r;
+  template <typename O> void operator()(O o, const char *in) const {
+    l(
+        [&](int len) {
+          if (len < 0)
+            o(len);
+          else
+            r(o, in + len);
+        },
+        in);
+  }
+};
+template <typename L, typename R> and_(L, R) -> and_<L, R>;
+
+template <char... Chrs> struct any {
+  void operator()(Car o, const char *b) {
+    if constexpr (((Chrs > 0x80) || ...))
+      ;
+    else
+      ;
+    o(((Chrs == b[0]) || ...) ? 1 : -1);
+  }
 };
 
-template <typename... Ts> struct print;
-template <char... Chrs> struct any {
-  void operator()(Car o, const char *b) { o(((Chrs == b[0]) || ...) ? 1 : -1); }
-};
 template <char... Chrs> struct none {
   void operator()(Car o, const char *b) { o(((Chrs != b[0]) && ...) ? 1 : -1); }
 };
+
+template <const char *Match> struct str {
+  void operator()(Car o, const char *in) const {
+    size_t i = 0;
+    while (char c = Match[i])
+      if (in[i++] != c)
+        return o(-1);
+    o(i);
+  }
+};
+
 int main() {
-  none<'i', 'f'>{}([](auto len) { std::cout << len << '\n'; }, "f");
-  ;
-  l(o::rec{[](Car rec, head_ray *, int i, Car tail) {
-    std::cout << i << ' ';
-    if constexpr (std::is_same_v<std::decay_t<decltype(tail)>,
-                                 decltype(nullptr)>)
-      ; // print<decltype(tail)> pp;
-    else
-      tail(rec);
-  }});
+  auto text = "g0t0<grammar::E41::E, grammar::E41::E>";
+
+  constexpr static const char abcაბვ[] = "abcაბვ";
+  using p = str<abcაბვ>;
+
+  p{}([](auto len) { std::cout << len << '\n'; }, text);
+
   return 9;
 }
