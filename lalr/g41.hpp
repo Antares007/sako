1798bfb27172fe876542e8ee340026e73cee9efc
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-02-17 11:27:14 +0400

    rename epsilon

--- lalr/g41.hpp
+++ lalr/g41.hpp
@@ -9,7 +9,7 @@ RG ( rparen )(b[0] == ')' ? 1 : -1);
 RGB( id     )(o = 0; for (; 'a' <= b[o] && b[o] <= 'z'; ++o); o = o ? o : -1;);
 RG ( a      )(b[0] == 'a' ? 1 : -1);
 RG ( b      )(b[0] == 'b' ? 1 : -1);
-RGB( Є      )((void(b)); o = 0;);
+RGB( Epsilon)((void(b)); o = 0;);
 RG ( dollar )(-b[0]);
 namespace E41 {
 v_( E )(L2(L3( E{},      plus{}, T{}      ), 
@@ -27,7 +27,7 @@ v_( E )(L4(L3( E{},      plus{}, E{}      ),
 }
 namespace ll_k_problem {
 v_( S )(L2(L2( S{}, A{} ), 
-           L1( Є{}      )))
+           L1( Epsilon{})))
 v_( A )(L1(L1( a{}      )))_v _v
 }
 namespace aabb {
